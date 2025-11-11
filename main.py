0207c842e448db07dbdcf4b0331f03c8f7330f3c
ABHISHEK B SHETTY <abhishetty1506@gmail.com>
2025-11-11 17:34:58 +0530

    using path parameter and query parameter

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ async def main():
 
 
 @app.get("/greet/{name}")
-async def greet(name: str) -> dict:
-    return {"message" : f"welcome {name}"}
+async def greet(name: str,age : int) -> dict:
+    return {"message" : f"welcome {name} your age is {age}"}
