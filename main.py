4a6ba02c202804476ff452c88c6ce944637eb21c
ABHISHEK B SHETTY <abhishetty1506@gmail.com>
2025-11-11 17:56:37 +0530

    added pydantic model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from typing import Optional
+from pydantic import BaseModel
 app = FastAPI()
 
 
@@ -11,3 +12,17 @@ async def main():
 @app.get("/greet")
 async def greet(name: Optional[str] = "user",age : Optional[int] = 0) -> dict:
     return {"message" : f"welcome {name} your age is {age}"}
+
+
+class BookCreate(BaseModel):
+    title : str
+    author: str
+
+
+@app.post('/create-book')
+async def createbook(book_data : BookCreate):
+    return {
+        "title":book_data.title,
+        "author":book_data.author
+    }
+
